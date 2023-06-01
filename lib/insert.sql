e34573fac0281d28e2f7ae142468e2de71bc85cf
tylerkirumba <Tyler>
2023-06-01 16:51:31 +0300

    bears lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate",10, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 8, "F", "white", "playful", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 7,"M", "light brown", "energetic", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown",10, "F", "red", "friendly", 0);
+INSERT INTO bears VALUES (5, "Melissa",4, "M", "blue", "chill", 1);
+INSERT INTO bears VALUES (6, "Grinch",9, "F", "green", "curious", 0);
+INSERT INTO bears VALUES (7, "Wendy",10, "M", "orange", "brave", 1);
+INSERT INTO bears VALUES (8, NULL, 10, "F", "yellow", "cheerful", 1);
